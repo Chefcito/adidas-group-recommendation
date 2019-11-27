077284cfce3a31494e92b7517c9f4c4724d91491
AnotherPotato <joansebastian35@gmail.com>
2019-11-27 15:40:38 -0500

    User selection system working

--- index.js
+++ index.js
@@ -1,40 +1,52 @@
 const PORT = 5500;
-
-const EXPRESS = require('express');
-const BODY_PARSER = require('body-parser');
-const HBS = require('express-handlebars');
-const MONGO_CLIENT = require('mongodb').MongoClient;
-
-const app = EXPRESS();
 const URL = 'mongodb://localhost:27017';
 const DATABASE_NAME = 'adidas_database';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const hbs = require('express-handlebars');
+const MongoClient = require('mongodb').MongoClient;
+const client = new MongoClient(URL, {useUnifiedTopology: true});
+
+const app = express();
 var database = null;
 
-const CLIENT = new MONGO_CLIENT(URL, {useUnifiedTopology: true});
 
-/* CLIENT.connect(function(err) {
+client.connect(function(err) {
     if(err) {
         console.error(err);
         return;
     } 
  
-    database = CLIENT.db(DATABASE_NAME);
-}); */
+    database = client.db(DATABASE_NAME);
+    app.listen(PORT, function() {
+        console.log("Server running on port " + PORT);
+    }); 
+});
 
-app.use(EXPRESS.static('public'));
+app.use(express.static('public'));
 
-app.engine('handlebars', HBS({
-    extname: "HBS",
+app.engine('handlebars', hbs({
+    extname: "hbs",
     defaultLayout: "",
     layoutsDir: "",
  }));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+//#region VARIABLES GLOBALES
+var selectedUser;
+//#endregion
+
 //#region RUTAS GET
 //Home
 app.get('/', function(request, response) {
 
-    CLIENT.connect(function(err) {
+    /* CLIENT.connect(function(err) {
         if(err) {
             console.error(err);
             response.send(err);
@@ -57,17 +69,50 @@ app.get('/', function(request, response) {
             
             response.render('home', context);
         });
+    }); */
+    
+    const collection = database.collection('users');
+    collection.find({}).toArray(function (err, docs) {
+        if(err) {
+            console.error(err);
+            response.send(err);
+            return;
+        }
+
+        var context = {
+            title: 'Titulazo',
+            users: docs,
+        }
+        
+        response.render('home', context);
     });
 }); 
 
 //Selección de Usuario
 app.get('/user-selection', function(request, response) {
-    response.render('user-selection');
+    const collection = database.collection('users');
+    collection.find({}).toArray(function (err, docs) {
+        if(err) {
+            console.error(err);
+            response.send(err);
+            return;
+        }
+
+        var context = {
+            users: docs,
+        }
+        
+        response.render('user-selection', context);
+    });
 });
 
 //Experiencia Adidas
 app.get('/adidas-experience', function(request, response) {
-    response.render('adidas-experience');
+    var context = {
+        userId: selectedUser,
+    }
+
+    response.render('adidas-experience', context);
 });
 
 //Recomendación Grupal
@@ -86,6 +131,12 @@ app.get('/item', function(request, response) {
 });
 //#endregion
 
-app.listen(PORT, function() {
-    console.log("Server running on port " + PORT);
-}); 
+//#region RUTAS POST
+// Seleccionar usuario
+app.post('/api/selectUser', function(request, response){
+    let userId = request.body.userId;
+    selectedUser = userId;
+
+    response.send(selectedUser);
+});
+//#endregion
--- public/scripts/user-selection.js
+++ public/scripts/user-selection.js
@@ -1,3 +1,36 @@
 window.addEventListener('load', function(){
+    var usersDropdown = document.querySelector('.users-dropdown');
+    var selectedUserId = usersDropdown.value;
 
+    usersDropdown.addEventListener('change', function(){
+        selectedUserId = usersDropdown.value;
+        console.log(selectedUserId);
+    });
+    
+
+    var startButton =   document.querySelector('.start-button');
+    startButton.addEventListener('click', function(){
+        selectUser(selectedUserId);
+    });
+
+    function selectUser(selectedUserId) {
+        fetch(`/api/selectUser`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/x-www-form-urlencoded'
+            },
+            body: `userId=${selectedUserId}`,
+        }).then(function(response){
+            return response.text();
+        }).catch(function(err){
+            console.error(err);
+        }).then(function(message){
+            console.log(message);
+            goToAdidasExperience();
+        });
+    }
+
+    function goToAdidasExperience() {
+        window.location.href = "/adidas-experience";
+    }
 });
